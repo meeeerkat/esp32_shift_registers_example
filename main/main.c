5733b063635450fb1f4553c37188b43dc964e1c8
Paul Suau <paul.suau@grenoble-inp.org>
2023-02-03 19:39:06 +0000

    Followed simplification of shift_registers component

--- main/main.c
+++ main/main.c
@@ -3,19 +3,15 @@
 #include "nvs_flash.h"
 #include "shift_register.h"
 
-static const char *TAG = "MAIN";
 
 
-
-shift_register_t sr;
 
 
 void app_main(void)
 {
-    init_shift_registers_handling();
+  shift_register_t sr;
+  shift_register__init(&sr, 16, 17, 18, 8);
 
-
-    shift_register__init(&sr, 16, 17, 18, 8);
-    // Bits [7,4] at 1, others at 0
-    shift_register__send(&sr, 0xF0);
+  // Bits [1,0] at 1, others at 0
+  shift_register__send(&sr, 0x03);
 }
