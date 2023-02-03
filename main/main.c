44e4653098de1ab879ba71cb083b952487840f75
Paul Suau <paul.suau@grenoble-inp.org>
2023-02-03 20:25:45 +0000

    Using shift_register__update + more tests

--- main/main.c
+++ main/main.c
@@ -2,6 +2,8 @@
 #include "esp_log.h"
 #include "nvs_flash.h"
 #include "shift_register.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 
 
 
@@ -14,4 +16,24 @@ void app_main(void)
 
   // Bits [1,0] at 1, others at 0
   shift_register__send(&sr, 0x03);
+
+  vTaskDelay(pdMS_TO_TICKS(1000));
+
+  // Update the first 4 bits to 1110
+  shift_register__update(&sr, 0xF, 0xE);
+
+  vTaskDelay(pdMS_TO_TICKS(1000));
+
+  // Update the last 4 bits to 1010
+  shift_register__update(&sr, 0xF0, 0xA0);
+
+  vTaskDelay(pdMS_TO_TICKS(1000));
+
+  // All on
+  shift_register__send(&sr, 0xFF);
+
+  vTaskDelay(pdMS_TO_TICKS(1000));
+
+  // All off
+  shift_register__send(&sr, 0x0);
 }
