66fb66447d17a86153ebce0136432871db46ae30
Paul Suau <paul.suau@grenoble-inp.org>
2023-02-22 22:59:46 +0000

    Using SPI driver shift_register component version + new test (counter from 0 to 255)

--- main/main.c
+++ main/main.c
@@ -7,33 +7,44 @@
 
 
 
-
-
-void app_main(void)
-{
-  shift_register_t sr;
-  shift_register__init(&sr, 16, 17, 18, 8);
-
+void basic_test(shift_register_t* sr) {
   // Bits [1,0] at 1, others at 0
-  shift_register__send(&sr, 0x03);
+  shift_register__send(sr, 0x03);
 
   vTaskDelay(pdMS_TO_TICKS(1000));
 
   // Update the first 4 bits to 1110
-  shift_register__update(&sr, 0xF, 0xE);
+  shift_register__update(sr, 0xF, 0xE);
 
   vTaskDelay(pdMS_TO_TICKS(1000));
 
   // Update the last 4 bits to 1010
-  shift_register__update(&sr, 0xF0, 0xA0);
+  shift_register__update(sr, 0xF0, 0xA0);
 
   vTaskDelay(pdMS_TO_TICKS(1000));
 
   // All on
-  shift_register__send(&sr, 0xFF);
+  shift_register__send(sr, 0xFF);
 
   vTaskDelay(pdMS_TO_TICKS(1000));
 
   // All off
-  shift_register__send(&sr, 0x0);
+  shift_register__send(sr, 0x0);
+}
+
+
+void counter(shift_register_t* sr) {
+  for (uint16_t c=0; c < 256; c++) {
+    shift_register__send(sr, c);
+    vTaskDelay(pdMS_TO_TICKS(100));
+  }
+}
+
+
+void app_main()
+{
+  shift_register_t sr;
+  shift_register__init(&sr, 16, 17, 18, 8);
+
+  counter(&sr);
 }
